d1f3eb7b5a7366bb1ae3a7cfe42025e8ba5f28b9
unknown <simojabri193@gmail.com>
2022-04-21 17:33:08 +0000

    first commit

---
+++ lib/simpelData/tipslist.dart
@@ -0,0 +1,11 @@
+const TipsList = [
+  {
+    "tips":"1-Open Youtube and copy the Youtube video URL you want to download.",
+  },
+  {
+    "tips":"2-Paste the Youtube video URL in the Search box, Tool will fetch video info.",
+  },
+  {
+    "tips":"3-After the, click the 'Download' button.",
+  },
+];
